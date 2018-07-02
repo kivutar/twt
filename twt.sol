345866b6a2588d4ceaaf52b62f5b0e6059af3cf5
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-07-02 15:34:16 +0700

    Support attaching images

--- twt.sol
+++ twt.sol
@@ -1,12 +1,13 @@
 pragma solidity ^0.4.11;
 contract Contract {
 
-    event Message(address indexed sender, uint id, string cid, uint timestamp);
+    event Message(address indexed sender, uint id, string cid, string img, uint timestamp);
     event Avatar(address indexed sender, string cid);
 
     struct Hash {
         address sender;
         string content;
+        string image;
         uint timestamp;
     }
 
@@ -17,13 +18,14 @@ contract Contract {
     constructor() public {
     }
 
-    function post(string _hashContent) public {
+    function post(string _hashContent, string _hashImage) public {
         uint id = ++lastHashId;
         hashes[id].sender = msg.sender;
         hashes[id].content = _hashContent;
+        hashes[id].image = _hashImage;
         hashes[id].timestamp = block.timestamp;
 
-        emit Message(hashes[id].sender, id, hashes[id].content, hashes[id].timestamp);
+        emit Message(hashes[id].sender, id, hashes[id].content, hashes[id].image, hashes[id].timestamp);
     }
 
     function set_avatar(string _hashAvatar) public {
